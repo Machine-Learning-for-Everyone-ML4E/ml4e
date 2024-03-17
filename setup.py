0dd9cbe0c82ceade62021e79de80d0ca63ca990c
Rakshit Kumar Singh <rakshitsingh421@gmail.com>
2024-03-17 19:55:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='ml4e',
     version='0.0.1.alpha.a',
     description='Home for state-of-art ML Models.',
+    url='https://github.com/Machine-Learning-for-Everyone-ML4E/ml4e',
     long_description='Home for state-of-art ML Models.',
     packages=find_packages(),
 )
