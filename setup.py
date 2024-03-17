cede984829fc09a99464a3022e7b9dec56bc5a53
Rakshit Kumar Singh <rakshitsingh421@gmail.com>
2024-03-17 19:26:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='ml4e',
     version='0.0.1',
-    long_description='Home for state-of-art ML Models.',
+    description='Home for state-of-art ML Models.',
     packages=find_packages()
 )
