c30304a5b2032eb2ee68006181b085e91febceb7
GreatRSingh <rakshitsingh421@gmail.com>
2024-04-11 19:50:29 +0530

    test

--- ml4e/.ipynb_checkpoints/__init__-checkpoint.py
+++ ml4e/.ipynb_checkpoints/__init__-checkpoint.py
@@ -2,3 +2,4 @@ from .main import hello
 import ml4e.plotters
 import ml4e.splitters
 import ml4e.datasets
+import ml4e.models
--- ml4e/datasets/data_loaders.py
+++ ml4e/datasets/data_loaders.py
@@ -6,5 +6,5 @@ def kaggleDataset(dataset_name):
     os.system('kaggle datasets download -d '+ dataset_name + ' -p '+ str(storage)+'/data/archive')
     zipPath = str(storage)+'/data/archive/'+dataset_name.split('/')[1]+'.zip'
     # Unzip
-    os.system('unzip '+zipPath+' -d '+str(storage)+'/data/'+dataset_name.split('/')[1])
+    os.system('unzip -n '+zipPath+' -d '+str(storage)+'/data/'+dataset_name.split('/')[1])
     return str(storage)+'/data/'+dataset_name.split('/')[1]
