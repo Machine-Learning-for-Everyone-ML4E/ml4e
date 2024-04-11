8d17be1b1923ee5506e0b8e869a87260ed8d81fc
GreatRSingh <rakshitsingh421@gmail.com>
2024-04-11 20:00:37 +0530

    fixes

--- ml4e/models/CNN_LSTM.py
+++ ml4e/models/CNN_LSTM.py
@@ -4,17 +4,17 @@ import torch.nn as nn
 class CNN_LSTM_Model(nn.Module):
     def __init__(self, num_classes):
         super(CNN_LSTM_Model, self).__init__()
-        
+
         # Convolutional layers
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
         self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.relu = nn.ReLU()
         self.flatten = nn.Flatten()
-        
+
         # LSTM layer
         self.lstm = nn.LSTM(input_size=64*7*7, hidden_size=128, num_layers=1, batch_first=True)
-        
+
         # Output layer
         self.fc = nn.Linear(128, num_classes)
 
