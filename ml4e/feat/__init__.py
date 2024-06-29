e32132ee64639a98b272240738803ceaa386c46b
GreatRSingh <rakshitsingh421@gmail.com>
2024-06-29 22:31:02 +0530

    smiles to coulomb matrix

---
+++ ml4e/feat/__init__.py
@@ -0,0 +1,6 @@
+try:
+    from ml4e.feat.coulomb_matrix import xyz_parse
+    from ml4e.feat.coulomb_matrix import gen_coulombmatrix
+    from ml4e.feat.coulomb_matrix import smi2cm
+except:
+    pass
